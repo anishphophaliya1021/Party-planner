899f9693d716705a849261fd7072f39be6fba5ed
aphophal <aphophal@andrew.cmu.edu>
2011-02-27 18:02:27 -0500

    started party planner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyPlanner::Application.config.secret_token = 'c05b4537a7e926b6a1dc3398b4eaaf86f998063f6edf1416458adfd09f2e19356e9eb43f249cf55d19acc3b83fee74ea4e456df1f7fffd818251778ba724d850'
